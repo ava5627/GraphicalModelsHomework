985e3fa92885262cab9eb9f3f011d142c4deef36
ava5627 <avasharris1@gmail.com>
2024-01-24 19:08:14 -0600

    python better progressbars

--- Homework 5/main.py
+++ Homework 5/main.py
@@ -1,8 +1,10 @@
 import sys
 import numpy as np
 import time
+import os
 from datetime import timedelta
 from concurrent.futures import ProcessPoolExecutor
+from tqdm import tqdm, trange
 
 
 def read_uai_file(filename):
@@ -40,19 +42,12 @@ def read_uai_file(filename):
         return cpt_vars, cpt_tables
 
 
-def print_table(cpt_vars, cpt_tables):
-    for f, v in zip(cpt_vars, cpt_tables):
-        print(f)
-        for i in v:
-            print(f"\t{i}")
-
-
 def maximum_liklihood_estimation(cpt_vars, training_file):
     data = np.loadtxt(training_file, skiprows=1, dtype=int)
 
     cpt_tables = []
 
-    for *parents, var in cpt_vars:
+    for *parents, var in tqdm(cpt_vars, desc="MLE         ", ncols=100, ascii="->=", colour="cyan"):
         cpt_values = []
 
         if not parents:
@@ -87,11 +82,11 @@ def gen_random_cpt_table(cpt_vars):
     return cpt_tables
 
 
-def probability_of_assignment(cpt_vars, cpt_tables, assignment):
+def probability_of_assignment(cpt_vars, cpt_tables, assignment: np.ndarray):
     prob = 1
     power_list = 2**np.arange(16)[::-1]
     for (*parents, var), table in zip(cpt_vars, cpt_tables):
-        parent_values = assignment[parents]
+        parent_values: np.ndarray = assignment[parents]
 
         # convert binary list to decimal index
         table_index = parent_values.dot(power_list[16-len(parents):])
@@ -101,39 +96,37 @@ def probability_of_assignment(cpt_vars, cpt_tables, assignment):
     return prob
 
 
-def log_likelihood_diff(cpt_vars, cpt_tables, learned_tables, test_file):
+def log_likelihood_diff(cpt_vars, cpt_tables, learned_tables, test_file, run_id=""):
     data = np.loadtxt(test_file, skiprows=1, dtype=int)
     ll_real = np.zeros(data.shape[0])
     ll_learned = np.zeros(data.shape[0])
-    avg_time = 0
-    for i in range(data.shape[0]):
-        start = time.time()
-        if i % 1000 == 0:
-            print(f"\rLL DIFF: {i:6}/{data.shape[0]}"
-                  f"                "
-                  f"{i/data.shape[0]:7.2%} "
-                  f"ETC: {timedelta(seconds=int(avg_time*(data.shape[0]-i)))}", end="")
+    bar_position = 2 * (run_id if run_id else 0)
+    for i in trange(data.shape[0], desc=f"LL DIFF {run_id:<4}", ncols=100, ascii="->=", colour="cyan", leave=False, position=bar_position, nrows=10):
         ll_real[i] = probability_of_assignment(cpt_vars, cpt_tables, data[i])
         ll_learned[i] = probability_of_assignment(cpt_vars, learned_tables, data[i])
-        avg_time = (avg_time * i + time.time() - start) / (i+1)
-    print()
     diff = np.abs(np.log(ll_real) - np.log(ll_learned)).sum()
     return diff
 
 
-def fill_data(data, cpt_vars, cpt_tables, msg, etc):
+def fill_data(data, cpt_vars, cpt_tables, bar_position=None):
     missing_values = (data == -1)
 
     missing_size = np.exp2(np.count_nonzero(missing_values, axis=1)).sum(dtype=int)
     filled_data = np.zeros((missing_size, data.shape[1]))
     data_weights = np.zeros(missing_size)
 
-    start = time.time()
     j = 0
-    for i, row in enumerate(data):
-        percent = j/missing_size/2
-        print(f"{msg} {percent:7.2%} ETC: {etc - timedelta(seconds=int(time.time() - start - 2))}", end="")
-
+    for i, row in tqdm(
+        enumerate(data),
+        desc="Fill Data   ",
+        ncols=100,
+        ascii="->=",
+        colour="cyan",
+        total=data.shape[0],
+        leave=False,
+        position=bar_position + 1,
+        nrows=10
+    ):
         # if no missing values, use the original data and a weight of 1
         if not missing_values.any():
             data_weights = np.append(data_weights, 1)
@@ -173,24 +166,17 @@ def expectation_maximization(cpt_vars, training_file, iterations=20, run_id=None
     cpt_tables = []
 
     cpt_tables = gen_random_cpt_table(cpt_vars)
-    avg_time = data.shape[0] / 100
-    for i in range(iterations):
-        start = time.time()
-        msg = f"\rEM run {run_id} |{'#'*(i + 1)}{' '*(iterations-i-1)}| {i+1:02}/{iterations:2}"
-        percent = 0
-        etc = timedelta(seconds=int(avg_time * (iterations-i)))
-        print(f"{msg} {percent:7.2%} ETC: {etc}", end="\x1b[K")
+    bar_position = 2 * (run_id if run_id is not None else 0)
+    for i in trange(iterations, desc=f"EM {run_id:<9}", ncols=100, ascii="->=", colour="cyan", position=bar_position, leave=False, nrows=10):
 
         # Fill in missing values and weight using the current cpt_tables
-        data_weights, filled_data = fill_data(data, cpt_vars, cpt_tables, msg, etc)
+        data_weights, filled_data = fill_data(data, cpt_vars, cpt_tables, bar_position)
         del cpt_tables
         cpt_tables = []
 
         j = 0
-        for *parents, var in cpt_vars:
-            percent = .5 + j/len(cpt_vars)/2
+        for *parents, var in tqdm(cpt_vars, desc="EM Iteration", ncols=100, ascii="->=", colour="cyan", leave=False, position=bar_position+1, nrows=10):
             j += 1
-            print(f"{msg} {percent:7.2%} ETC: {etc - timedelta(seconds=int(time.time() - start - 2))}", end="")
 
             if not parents:
                 numerator_indices = (filled_data[:, var] == 0)
@@ -215,8 +201,6 @@ def expectation_maximization(cpt_vars, training_file, iterations=20, run_id=None
                 cpt_values = np.hstack([thetas, 1 - thetas])
 
             cpt_tables.append(cpt_values)
-        avg_time = (avg_time * i + time.time() - start) / (i+1)
-    print(end=' ')
     return cpt_tables
 
 
@@ -224,14 +208,8 @@ def run(input_uai_filename, task_id, training_file, test_file, parallel=False):
     parallel = int(parallel)
     cpt_vars, cpt_tables = read_uai_file(input_uai_filename)
     if task_id in ['f', '1']:
-        start = time.time()
         learned_tables = maximum_liklihood_estimation(cpt_vars, training_file)
-        learning_time = time.time() - start
-        print(f'Finished learning in {timedelta(seconds=int(learning_time))}')
         diff = log_likelihood_diff(cpt_vars, cpt_tables, learned_tables, test_file)
-        diff_time = time.time() - start - learning_time
-        print(f'Finished in {timedelta(seconds=int(diff_time))}')
-        print(f'Log-likelihood difference: {diff}')
         return f"Log Likelihood Difference = {diff}"
 
     elif task_id in ['p', '2'] and not parallel:
@@ -240,7 +218,6 @@ def run(input_uai_filename, task_id, training_file, test_file, parallel=False):
             diffs[i] = run_em(cpt_vars, cpt_tables, training_file, test_file, i)
         mean = diffs.mean()
         stdev = diffs.std()
-        print(f"Log Likelihood Difference: {mean} +/- {stdev}")
         return f"Log Likelihood Difference = {mean} +/- {stdev}"
     elif task_id in ['p', '2'] and parallel:
         with ProcessPoolExecutor(max_workers=5) as executor:
@@ -250,31 +227,34 @@ def run(input_uai_filename, task_id, training_file, test_file, parallel=False):
             diffs = [future.result() for future in futures]
         mean = np.mean(diffs)
         stdev = np.std(diffs)
-        print(f"Log Likelihood Difference: {mean} +/- {stdev}")
         return f"Log Likelihood Difference = {mean} +/- {stdev}"
     else:
         sys.exit(f'Task id must be 1, 2, f, or p not {task_id}')
 
 
 def run_em(cpt_vars, cpt_tables, training_file, test_file, run_id):
-    print(f"Starting run: {run_id}")
-    start = time.time()
     learned_tables = expectation_maximization(cpt_vars, training_file, iterations=20, run_id=run_id)
-    learning_time = time.time() - start
-    print(f'{run_id} finished learning in {timedelta(seconds=int(learning_time))}')
-    diff = log_likelihood_diff(cpt_vars, cpt_tables, learned_tables, test_file)
-    diff_time = time.time() - start - learning_time
-    print(f'{run_id} Finished with in {timedelta(seconds=int(diff_time))}')
-    print(f'{run_id} Log-likelihood difference: {diff}')
+    diff = log_likelihood_diff(cpt_vars, cpt_tables, learned_tables, test_file, run_id=run_id)
     return diff
 
 
 def main():
-    if len(sys.argv) not in [5, 6]:
-        sys.exit("python3 main.py <input-uai-file> <task-id> <training-data> <test-data> [parallel]")
+    if len(sys.argv) < 4 or (sys.argv[2] == 'p' and len(sys.argv) != 5):
+        sys.exit("python3 main.py dataset mode file parallel")
 
-    _, input_uai_filename, task_id, training_file, test_file, *parallel = sys.argv
-    print(run(input_uai_filename, task_id, training_file, test_file, *parallel))
+    _, dataset, task_id, training_file, *parallel = sys.argv
+    input_uai_filename = f"./hw5-data/dataset{dataset}/{dataset}.uai"
+    training_file = f"./hw5-data/dataset{dataset}/train-{task_id}-{training_file}.txt"
+    test_file = f"./hw5-data/dataset{dataset}/test.txt"
+    if not os.path.exists(input_uai_filename):
+        sys.exit(f"Dataset {dataset} does not exist")
+    if not os.path.exists(training_file):
+        sys.exit(f"Training file {training_file} does not exist")
+    if not os.path.exists(test_file):
+        sys.exit(f"Test file {test_file} does not exist")
+    start = time.time()
+    print("\n"*10, run(input_uai_filename, task_id, training_file, test_file, *parallel))
+    print(f"Finished in {timedelta(seconds=int(time.time() - start))}")
 
 
 if __name__ == "__main__":
